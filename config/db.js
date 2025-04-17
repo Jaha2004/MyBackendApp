047b2c09159a7571e98e489f16b0e4e33b32c534
Soudarjya <soudyg707@gmail.com>
2025-04-17 11:39:14 +0530

    db issues

--- config/db.js
+++ config/db.js
@@ -7,8 +7,9 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/DB');
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB Connected');
+        
     } catch (error) {
         console.error('MongoDB connection error:', error);
         process.exit(1);
